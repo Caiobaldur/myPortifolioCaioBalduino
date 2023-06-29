de1f5be7483b64cd566107791fb029092a9f7f52
Caiobaldur <caiobrafachino@hotmail.com>
2023-06-29 00:40:53 -0300

    slider-click-and-pull-js [mobile-first]

--- script.js
+++ script.js
@@ -7,27 +7,33 @@ const controlsWrapper = document.querySelector('[data-slide="controls-wrapper"]'
 const slideItens = document.querySelectorAll('[data-slide="item"]')
 const controlButtons = document.querySelectorAll('[data-slide="control-button"]')
 
+let startingPoint
+let savedPosition
 
-function onMouseMove() {
 
+
+function onMouseDown(event) {
+  const slideItem = event.currentTarget
+  startingPoint = event.clientX
+  slideItem.addEventListener('mousemove', onMouseMove)
 }
 
 
+function onMouseMove(event) {
+  const movement = event.clientX - startingPoint
+  slideList.style.transform = 'translateX('+movement+'px)'
+  savedPosition = movement
+}
 
-
+function onMouseUp(event){
+  const slideItem = event.currentTarget
+  slideItem.removeEventListener('mousemove', onMouseMove)
+}
 
 slideItems.forEach(function(slideItem, index) {
   slideItem.addEventListener('dragstart', function(event) {
     event.preventDefault()
   })
-  slideItem.addEventListener('mousedown', function() {
-
-    slideItem.addEventListener('mousemove', onMouseMove)
-    
-  
-  })
-  slideItem.addEventListener('mouseup', function() {
-    slideItem.removeEventListener('mousemove', onMouseMove)
-  })
-  
+  slideItem.addEventListener('mousedown',onMouseDown)
+  slideItem.addEventListener('mouseup', onMouseUp)
 })
